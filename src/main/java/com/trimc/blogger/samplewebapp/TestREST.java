d2c27ad0cd91afc33c5ba8769416bd78b14287e2
jayarul <karul43@yahoo.co.in>
2016-07-10 20:03:51 +0100

    update for JavaRestSample

--- src/main/java/com/trimc/blogger/samplewebapp/TestREST.java
+++ src/main/java/com/trimc/blogger/samplewebapp/TestREST.java
@@ -6,23 +6,12 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
-@Path("/rest")
-public class TestREST {
-
-	class Result {
-		public String name;
-		public long timestamp;
-
-		public Result(String name) {
-			this.timestamp = System.currentTimeMillis();
-			this.name = name;
-		}
-	}
+@Path("/")
+public class TestREST {	
 
 	@GET
-	@Path("/get")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Object getit(@QueryParam("name") String name) {
-		return new Result(name);
+	@Path("/getResponse")
+	public Object getit() {
+		return "Response from REST service in docker container !!!";
 	}
 }
